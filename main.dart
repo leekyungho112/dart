40bede045f5e76f77bb50ba29fd0f672d7d593ed
leekyungho <epsoe@naver.com>
2025-03-15 17:55:30 +0900

    List

--- main.dart
+++ main.dart
@@ -1,8 +1,4 @@
 void main() {
-  String name = "lee";
-  bool live = true;
-  int age = 19;
-  double money = 88.99;
-  num a = 1.22;
-  a = 33;
+  var giveMeFive = true;
+  var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
 }
