fd6184d8deb9bc1c365e867c29621483b3878272
leekyungho <epsoe@naver.com>
2025-03-18 13:27:29 +0900

    map

--- main.dart
+++ main.dart
@@ -1,5 +1,3 @@
 void main() {
-  var oldFriend = ["lee", "lee2"];
-  var newFriend = ['choi', "kim", "park", for (var friend in oldFriend) friend];
-  print(newFriend);
+  var maps = {"age": 17, "name": "lee", "man": true};
 }
